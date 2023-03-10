ad13408c5f748fb74f567472000f69d044f1b8f3
jakubir <irlajakub@gmail.com>
2023-03-10 15:26:15 +0100

    obsługa błędów

--- src/components/Messy.js
+++ src/components/Messy.js
@@ -1,27 +1,56 @@
 import React, { useState } from "react";
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Messy() {
-    const [link, setLink] = useState('');
-    const navigate = useNavigate();
+  const [link, setLink] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
-    return (
-        <form className="Form" onSubmit={(e) => {
-            e.preventDefault();
-            axios.post('http://localhost:3001/link/link', {'link': link})
-                .then((res) => navigate(`/tiny/${res.data}`));
-        }}>
-            <h3>Link:</h3>
-            <label>
-                <input required type="text" name="url" className="link" value={link} 
-                onChange={(e) => {
-                setLink(e.target.value);
-                }}/>
-            </label>
-            <input type="submit"/>
-        </form>
-    );
+  return (
+    <>
+      <form
+        className="Form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          axios.post("http://localhost:3001/link/link", { link: link })
+            .then((res) => {
+              switch (res.data.error) {
+                case 'dbConnection':
+                  setError('Brak połączenia z bazą danych');
+                  setTimeout(window.location.reload(), 10 * 1000);
+                  break;
+                case 'notALink':
+                  setError('Podany link jest niepoprawny');
+                  break;
+                default:
+                  navigate(`/tiny/${res.data.content}`);
+                  break;
+              }
+            })
+            .catch((error) => {
+              setError('Brak połączenia z serwerem');
+            });
+        }}
+        >
+        <h3>Link:</h3>
+        <label>
+          <input
+            required
+            type="text"
+            name="url"
+            className="link"
+            value={link}
+            onChange={(e) => {
+              setLink(e.target.value);
+            }}
+            />
+        </label>
+        <input type="submit" />
+      </form>
+      <div className="container" style={{color: 'red'}}>{error}</div>
+      </>
+  );
 }
 
 export default Messy;
--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -1,23 +1,47 @@
-import React, { useEffect } from "react";
-import { useParams, useNavigate} from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
 
 function Redirect() {
-    const hash = useParams();
-    const navigate = useNavigate();
+  const hash = useParams();
+  const [link, setLink] = useState('');
+  const [error, setError] = useState('');
 
-    useEffect(() => {
-        axios.post('http://localhost:3001/hash/hash', {'hash': hash.h})
-            .then((res) => {
-                window.location.replace(res.data);
-            })
-    }, [hash, navigate])  
+  useEffect(() => {
+    axios.post("http://localhost:3001/hash/hash", { hash: hash.h })
+      .then((res) => {
+        if (res.data.error === 'unknownHash')
+          setError('Nieprawidłowy link');
+        else if (res.data.error === 'dbConnection') {
+          setError('Brak połączenia z bazą danych');
+          setTimeout(window.location.reload(), 10 * 1000);
+        } else {
+          setLink(res.data.content);
+          setTimeout(() => {
+            window.location.replace(res.data.content);
+          }, 3 * 1000);
+        }
+      })
+      .catch((error) => {
+        setError('Brak połączenia z serwerem');
+      });
+  }, [hash]);
 
+  if (error !== '') {
+    
     return (
-        <div className="Container">
-            Dziękujemy za korzystanie z naszej strony :D
-        </div>
+      <div className="container">
+        {error}. Przejdź na stronę główną i<Link to={'/'}> spróbuj stworzyć swój własny, krótki link.</Link>
+      </div>
     );
+  }
+
+  return (
+    <div className="container">
+      Dziękujemy za korzystanie z naszeg serwisu :D <br/> 
+      Następuje przekierowanie do strony: {link}
+    </div>
+  );
 }
 
 export default Redirect;
